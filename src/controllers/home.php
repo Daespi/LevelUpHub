6ef5beb4012fa6b427e59b2ab7b30447025dccf9
daespi <d.espinosa1smixnuria2020@gmail.com>
2024-10-04 18:15:55 +0200

    dev1

---
+++ src/controllers/home.php
@@ -0,0 +1,21 @@
+<?php
+
+
+require MODS.'/home.php';
+//require 'src/database.php';
+
+$db = connectMysql($dsn,$dbuser,$dbpassword);
+
+//dd($dbpassword);
+//$sql = "SELECT * FROM books";
+$users = query($db , "SELECT * FROM users");
+
+
+/*$stmt = $db->prepare($sql);
+$stmt -> execute();
+$users = $stmt->fetchALL();
+//var_dump($users);
+//dd($books);*/
+
+
+?>
---
+++ src/database/configMysql.php
@@ -0,0 +1,11 @@
+<?php
+<?php
+$dbhost= 'localhost';
+$dbname = 'levelUpHubBD';
+$dbuser = 'dev1';
+$dbpassword ='linuxlinux';
+$dsn = 'mysql:host='.$dbhost.';dbname='.$dbname.';';
+
+
+?>
+
